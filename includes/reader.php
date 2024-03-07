6a3f6f1c5a6aba5d534a3e52909f0c83d7634e2e
John Coonrod <john@mcld.org>
2024-03-07 13:08:42 -0500

    fix access point

--- includes/reader.php
+++ includes/reader.php
@@ -1,13 +1,13 @@
 <?php
 if(LOCAL) {
 	$db=new PDO('mysql:host=localhost;dbname=mcld','reader','SDG2030');
-	$email="john.coonrod@thp.org";
+	$email="john@mcld.org";
 	$name='Local admin';
 	$user=$email;
 	setcookie('user',$user,0,'/');$_COOKIE['user']=$user; 
 	setcookie('name',$name,0,'/');$_COOKIE['name']=$name; 
 } else {
-	$db = new PDO("mysql:unix_socket=/cloudsql/thpbudget:us-east1:thpbudget;dbname=mcld","reader","SDG2030");
+	$db = new PDO("mysql:unix_socket=/cloudsql/mcldtime:us-central1:mcld;dbname=mcld","reader","SDG2030");
 	$email=strtolower($_SERVER['HTTP_X_APPENGINE_USER_EMAIL']);
 	$user=$email;
 	$name=$_SERVER['HTTP_X_APPENGINE_USER_NICKNAME'];
